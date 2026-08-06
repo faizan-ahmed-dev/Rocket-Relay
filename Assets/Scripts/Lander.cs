7058203004681102c0a691959f92b2482df48edd
Faizy <mehunfaizan@gmail.com>
2026-08-06 11:05:06 +0500

    Added Landing Detection

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -32,4 +32,23 @@ public class Lander : MonoBehaviour
 
     }
 
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        float softLandingVelocityMagnitude = 4f;
+        if (collision2D.relativeVelocity.magnitude > softLandingVelocityMagnitude)
+        {
+            Debug.Log("Landed too fast");
+            return;
+        }
+
+        float dotVector = Vector2.Dot(Vector2.up, transform.up);
+        float minDotVector = .90f;
+        if (dotVector < minDotVector)
+        {
+            Debug.Log("Landed on a too steep angle!");
+        }
+
+        Debug.Log("Phew.. Soft Landing"); 
+    }
+
 }
